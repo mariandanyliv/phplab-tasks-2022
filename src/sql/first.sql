d58e478ab4e12eb7a795a78ae7b7b15d9dd88c4e
mariandanyliv <danulivmarian40@gmail.com>
2022-09-19 19:57:47 +0300

    [SQL] sql fix

--- src/sql/first.sql
+++ src/sql/first.sql
@@ -3,19 +3,19 @@ CREATE DATABASE hospital;
 CREATE TABLE diseases
 (
     id          	INT UNSIGNED  AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    nameDiseases    VARCHAR NOT NULL
+    name            UNIQUE VARCHAR
 );
 
-INSERT INTO diseases (nameDiseases)
+INSERT INTO diseases (name)
 VALUES ('firstdiseases'), ('seconddiseases'), ('thirddiseases');
 
 CREATE TABLE medicines
 (
     id                      INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name                    VARCHAR(50) NOT NULL,
-    amount			        INT, NOY NULL,
-    diseasesId              INT UNSIGNED NOT NULL,
-    FOREIGN KEY (diseasesId) REFERENCES diseases(id)
+    amount			        INT, NOT NULL,
+    diseases_id             INT UNSIGNED NOT NULL,
+    FOREIGN KEY (diseases_id) REFERENCES diseases(id)
 );
 
 INSERT INTO medicines(name, amount, diseasesId)
@@ -24,12 +24,39 @@ VALUES ('firstMedicinesa', 50, 3), ('secondMedicinesa', 70, 2), ('thirdMedicines
 CREATE TABLE patients
 (
     id                    INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    firstName             VARCHAR(50) NOT NULL,
-    lastName              VARCHAR(50) NOT NULL,
-    gender                CHAR NOT NULL,
-    medicinesId           INT UNSIGNED NOT NULL,
+    first_name            VARCHAR(50) NOT NULL,
+    last_name              VARCHAR(50) NOT NULL,
+    gender                BOOLEAN  NOT NULL,
+    medicines_id          INT UNSIGNED NOT NULL,
     FOREIGN KEY (medicinesId) REFERENCES medicines(id)
 );
 
 INSERT INTO patients (firstName, lastName, gender, medicinesId)
 VALUES ('Marian','Danyliv', 'mal' 1), ('Tanuya','Ivanisha', 'fem' 2), ('Volodia','Danyliv', 'mal' 3);
+
+CREATE TABLE IF NOT EXISTS `diseases_medicines` (
+    `diseases_id` INT NOT NULL,
+    `medicines_id` INT NOT NULL,
+    FOREIGN KEY (`diseases_id`) REFERENCES `diseases`(`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+    FOREIGN KEY (`medicines_id`) REFERENCES `medicines`(`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+    );
+
+CREATE TABLE IF NOT EXISTS `diseases_patients` (
+    `diseases_id` INT NOT NULL,
+    `patients_id` INT NOT NULL,
+    FOREIGN KEY (`diseases_id`) REFERENCES `diseases`(`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+    FOREIGN KEY (`patients_id`) REFERENCES `patients`(`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+);
+CREATE TABLE IF NOT EXISTS `patients_diseases` (
+    `patients_id` INT NOT NULL,
+    `diseases_id` INT NOT NULL,
+    FOREIGN KEY (`diseases_id`) REFERENCES `diseases`(`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+    FOREIGN KEY (`patients_id`) REFERENCES `patients`(`id`) ON UPDATE CASCADE ON DELETE CASCADE;
+);
+
+INSERT INTO diseases_medicines (diseases_id, medicines_id)
+VALUES (1,2), (2,3), (3,1), (3,2);
+INSERT INTO diseases_patients (diseases_id, patients_id)
+VALUES (1,2), (2,3), (3,1), (3,2);
+INSERT INTO patients_diseases (patients_id, diseases_id)
+VALUES (1,2), (2,3), (3,1), (3,2);
--- src/sql/second.sql
+++ src/sql/second.sql
@@ -2,15 +2,15 @@ SELECT name, amount FROM medicines WHERE amount > 25 ORDER BY amount;
 
 SELECT COUNT(name) FROM medicines WHERE amount > 50;
 
-SELECT nameDiseases FROM  diseases
+SELECT name FROM  diseases
 UNION
 SELECT name FROM medicines
 
 SELECT name, amount FROM medicines
 WHERE amount > (SELECT AVG(amount) FROM medicines) ORDER BY amount;
 
-SELECT patients.firstName, patients.lastName AS patients, medicines.name, medicines.amount AS medicines, diseases.name AS diseases
+SELECT patients.first_name, patients.last_name AS patients, medicines.name, medicines.amount AS medicines, diseases.name AS diseases
 FROM models
-         LEFT JOIN medicines ON medicines.diseasesId = medicines.id
-         LEFT JOIN diseases ON medicines.manufacturer_id = diseases.id
+         LEFT JOIN diseases_medicines ON medicines.id = diseases_medicines.medicines_id
+         LEFT JOIN diseases_medicines ON diseases.id = diseases_medicines.diseases_id
 ORDER BY medicines.name;
